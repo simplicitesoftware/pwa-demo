e5c589e7159f410949e0b57fdbb657b1e32ae90b
Simplicite Software <contact@simplicitesoftware.com>
2023-11-06 18:07:26 +0100

    Milestone

--- app/script.js
+++ app/script.js
@@ -1,14 +1,19 @@
-(async () => {
+window.addEventListener('load', async () => {
   if ('serviceWorker' in navigator) {
     try {
       const registration = await navigator.serviceWorker.register('/service-worker.js');
       console.log('Service worker registration succeeded:', registration);
+      render();
     } catch (error) {
       console.error(`Registration failed with ${error}`);
     }
   }
-})();
-
-window.addEventListener('load', event => {
-    document.getElementById('catalog').innerHTML = 'TODO: load catalog';
 });
+
+async function render() {
+    console.log('Rendering...');
+    const app = simplicite.session({ url: 'https://demo.dev.simplicite.io', debug: true });
+    const user = await app.login({ username: 'website', password: 'simplicite' });
+    console.log(user);
+    document.getElementById('catalog').innerHTML = `Hello ${user.login}`;
+}
--- app/service-worker.js
+++ app/service-worker.js
@@ -2,7 +2,7 @@ self.addEventListener('install', event => {
     console.log('Service worker: install', event);
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', async event => {
     console.log('Service worker: fetch', event);
     return fetch(event.request);
 });
