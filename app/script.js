e33d04c4f4cfddd1fc4b312bca55d5f460577fa4
Simplicite Software <contact@simplicitesoftware.com>
2023-11-06 23:22:57 +0100

    Milestone

--- app/script.js
+++ app/script.js
@@ -1,28 +1,32 @@
 let app;
 
 window.addEventListener('load', async () => {
-  if ('serviceWorker' in navigator) {
-    try {
-      await navigator.serviceWorker.register('/service-worker.js');
-      app = simplicite.session({ url: 'https://demo.dev.simplicite.io' });
-      document.getElementById('refresh').addEventListener('click', loadCatalog);
-      loadCatalog();
-    } catch (err) {
-      console.error(`Service worker registration failed: ${err}`);
+    if ('serviceWorker' in navigator) {
+        try {
+           await navigator.serviceWorker.register('/service-worker.js');
+           app = simplicite.session({ url: 'https://demo.dev.simplicite.io' });
+           document.getElementById('refresh').addEventListener('click', loadCatalog);
+           loadCatalog();
+        } catch (err) {
+            console.error(`Service worker registration failed: ${err}`);
+        }
     }
-  }
 });
 
 async function loadCatalog() {
     const catalog = document.getElementById('catalog');
     catalog.innerHTML = 'Loading...';
-    let html = '';
-    for (const prd of await app.getBusinessObject('DemoProduct').search({ demoPrdAvailable: true }, { inlineDocuments: [ 'demoPrdPicture' ] }))
-      html += `<div class="product">
-        <img src="data:${prd.demoPrdPicture.mime};base64,${prd.demoPrdPicture.content}"/>
-        <h1>${prd.demoPrdName}</h1>
-        <h2>${prd.demoPrdReference}</h2>
-        <p>${prd.demoPrdDescription}</p>
-        </div>`;
-    catalog.innerHTML = html;
+    try {
+        let html = '';
+        for (const prd of await app.getBusinessObject('DemoProduct').search({ demoPrdAvailable: true }, { inlineDocuments: [ 'demoPrdPicture' ] }))
+            html += `<div class="product">
+                <img src="data:${prd.demoPrdPicture.mime};base64,${prd.demoPrdPicture.content}"/>
+                <h1>${prd.demoPrdName}</h1>
+                <h2>${prd.demoPrdReference}</h2>
+                <p>${prd.demoPrdDescription}</p>
+                </div>`;
+        catalog.innerHTML = html;
+    } catch (err) {
+        catalog.innerHTML = `Error: ${err}`;
+    }
 }
--- app/service-worker.js
+++ app/service-worker.js
@@ -1,9 +1,40 @@
+const addResourcesToCache = async resources => {
+    const cache = await caches.open('pwa-demo');
+    await cache.addAll(resources);
+};
+
+const getResourceFromCache = async request => {
+    const resource = await caches.match(request);
+    return resource || fetch(request);
+};
+
 self.addEventListener('install', event => {
-    console.log('Service worker: install', event);
-    self.skipWaiting();
+    console.log('Service worker install', event);
+    event.waitUntil(addResourcesToCache([
+        '/',
+        '/manifest.json',
+        '/index.html',
+        '/styles.css',
+        '/script.js',
+        '/simplicite.min.js',
+        '/images/logo.svg',
+        '/icons/favicon.png',
+        '/icons/icon32.png',
+        '/icons/icon48.png',
+        '/icons/icon64.png',
+        '/icons/icon96.png',
+        '/icons/icon120.png',
+        '/icons/icon128.png',
+        '/icons/icon144.png',
+        '/icons/icon152.png',
+        '/icons/icon167.png',
+        '/icons/icon180.png',
+        '/icons/icon192.png',
+        '/icons/icon256.png'
+    ]));
 });
 
-self.addEventListener('fetch', event => {
-    console.log(`Service worker: fetch ${event.request.url}`, event);
-    return fetch(event.request);
+self.addEventListener('fetch', async event => {
+    console.log(`Service worker fetch ${event.request.url}`, event);
+    event.respondWith(getResourceFromCache(event.request));
 });
