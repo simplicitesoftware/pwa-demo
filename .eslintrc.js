04bc5825718c8eb8fa5dc192fe4d3ade80df4f84
David AZOULAY <dazoulay@simplicite.fr>
2024-01-05 18:27:48 +0100

    Bump versions

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,21 @@
 module.exports = {
-  'parserOptions': {
-		'ecmaVersion': 11
-  },
-  'env': {
-    'browser': true,
-    "es6": true
-  },
-  'extends': [
-    'eslint:recommended',
-  ],
-  'rules': {
-    'semi': ['error', 'always'],
-    'quotes': ['error', 'single'],
-    'no-multiple-empty-lines': ['error', { 'max': 1 }],
-    'indent': ['error', 4],
-  },
-  'ignorePatterns': [
-    'app/simplicite.min.js*'
-  ]
+    'parserOptions': {
+        'ecmaVersion': 11
+    },
+    'env': {
+        'browser': true,
+        'es6': true
+    },
+    'extends': [
+        'eslint:recommended',
+    ],
+    'rules': {
+        'semi': ['error', 'always'],
+        'quotes': ['error', 'single'],
+        'no-multiple-empty-lines': ['error', { 'max': 1 }],
+        'indent': ['error', 4],
+    },
+    'ignorePatterns': [
+        'app/simplicite.min.js*'
+    ]
 };
